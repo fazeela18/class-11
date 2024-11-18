3bac9e94acc69965a54df0e8a553ae65324afa45
fazeela18 <ansarifazeela616@gmail.com>
2024-11-18 16:00:29 +0500

    first commit

---
+++ app/api/external/route.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+import { json } from "stream/consumers";
+
+const EXTERNAL_API_URL ="https://jsonplaceholder.typicode.com/posts"
+
+export async function GET() {
+    try {
+const response = await fetch (EXTERNAL_API_URL)
+if(!response.ok){
+    return NextResponse.json(
+        {success:false,messsage:"Fetch the data from the API"},
+        {status:response.status}
+    )
+}
+const data =await response.json()
+
+return NextResponse.json({success:true,data}
+)
+    }catch (error:any) {
+        return NextResponse.json({success:false,message:"get the error!",error:error.message})
+    }
+    
+}
